9cacd367fd9f7a9583c52375f9c443cc9cc4e4df
Matheus-Salinas <matheussalinonhas@gmail.com>
2026-04-15 15:57:23 -0300

    fix(terraform-main): Corrigindo erro de deploy duplicando serviços já criados

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
+
+terraform {
+  backend "gcs" {
+    bucket  = "youtube-etl-gcp-terraform-state"
+    prefix  = "terraform/state"
+  }
+}
 provider "google" {
     project = var.project_id
     region = var.region
